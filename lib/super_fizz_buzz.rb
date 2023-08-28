10611714bc09eda0633142f48273bb3ec22cb0b9
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-28 08:40:27 -0600

    Correct tests and pass all

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,30 @@
 # Iterate through the numbers 1 to 1000
 # For each one, print the output according to the rules of Super Fizz Buzz
+def superfizzbuzz(number)
+  string = ''
+  if number % 7 != 0 && number % 3 != 0 && number % 5 != 0
+    string = number
+  else
+    if number % 7 == 0
+      string += 'Super'
+    end
+    if number % 3 == 0
+      string += 'Fizz'
+    end
+    if number % 5 == 0
+      string += 'Buzz'
+    end
+  end
+  string
+end
+
+num = 1
+array = []
+1000.times do
+  array << num
+  num += 1
+end
+
+array.each do |num|
+  p superfizzbuzz(num)
+end
--- spec/iteration_1_spec.rb
+++ spec/iteration_1_spec.rb
@@ -1,36 +1,36 @@
-require './lib/iteration_1'
+require './lib/super_fizz_buzz'
 
 RSpec.describe 'Super Fizz Buzz' do
   describe 'It will print numbers based on divisibility' do
     it 'will print Fizz if divisible by 3' do
       sfb = superfizzbuzz(3)
-      expect(sfb).to eq(Fizz)
+      expect(sfb).to eq("Fizz")
     end
 
     it 'will print Buzz if divisible by 5' do
       sfb = superfizzbuzz(5)
-      expect(sfb).to eq(Buzz)
+      expect(sfb).to eq("Buzz")
     end
     
     it 'will print Super if divisibl by 7' do
       sfb = superfizzbuzz(7)
-      expect(sfb).to eq(Super)
+      expect(sfb).to eq("Super")
     end
   end
 
   describe 'It will print some combination if divisible by multiple' do
     it 'will print in the order of SuperFizzBuzz' do
       sfb = superfizzbuzz(105)
-      expect(sfb).to eq(SuperFizzBuzz)
+      expect(sfb).to eq("SuperFizzBuzz")
     end
 
     it 'can print just two' do
       sfb = superfizzbuzz(15)
-      expect(sfb).to eq(FizzBuzz)
+      expect(sfb).to eq("FizzBuzz")
       sfb = superfizzbuzz(35)
-      expect(sfb).to eq(SuperBuzz)
+      expect(sfb).to eq("SuperBuzz")
       sfb = superfizzbuzz(21)
-      expect(sfb).to eq(SuperFizz)
+      expect(sfb).to eq("SuperFizz")
     end
   end
 
